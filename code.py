e37630654a0c5602d3189b7b960b95830cbed48a
EugeneFoxWox <98107337+EugeneFoxWox@users.noreply.github.com>
2022-01-31 22:09:43 +0300

    def dict_way_size

--- code.py
+++ code.py
@@ -6,7 +6,12 @@ def dir():
 
 
 def dict_way_size(way):
-    pass
+    dict1 = {}
+    for root, dirs, files in os.walk(way):
+        for file in files:
+            newkey = {os.path.join(root, file): os.path.getsize(root + '\\' + file)}
+            dict1.update(newkey)
+    return dict1
 
 def analysis(dict1):
     pass
