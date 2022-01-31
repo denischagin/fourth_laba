561c333b16c7d791914798fb781cd45a90bd76e2
Aleksandr90210 <98108166+Aleksandr90210@users.noreply.github.com>
2022-01-31 22:40:05 +0300

    Юдин Александр

--- code.py
+++ code.py
@@ -2,7 +2,12 @@ import os
 
 
 def dir():
-    pass
+    while True:
+        way = input('Введите директорию:')
+        if os.path.isdir(way) == True:
+            return way
+        else:
+            print('Вы ввели не правильный путь к файлу')
 
 
 def dict_way_size(way):
